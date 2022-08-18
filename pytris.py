02a517b3dcf62709c656af61c449c3ebb59dd081
Ryan Chiu <ryanchiu@uw.edu>
2022-08-17 22:52:54 -0700

    done

--- pytris.py
+++ pytris.py
@@ -12,16 +12,8 @@ def main():
     pygame.draw.line(game_manager.window, (255, 255, 255), (16, 621), (320, 621), 4)  # bottom border
     pygame.draw.line(game_manager.window, (255, 255, 255), (320, 16), (320, 623), 4)  # right border
 
-
-    # game_manager.window.blit(start_text, (game_manager.window_width / 2 + 50, game_manager.window_height / 2))
-
     game_manager.start_program()
-
-    # game_manager.start_game()
 
 
 if __name__ == "__main__":
     main()
-
-# Done with removing rows and stacking up. In progress for the rest such as
-# game over, etc.
